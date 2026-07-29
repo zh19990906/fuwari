953b5aeedb4ce7d188118983235885ec7aa015d6
努力奋斗的阿三 <59323683+zh19990906@users.noreply.github.com>
2026-07-29 17:52:36 +0800

    chore: configure Astro for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,8 +26,8 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://fuwari.vercel.app/",
-	base: "/",
+	site: "https://zh19990906.github.io",
+	base: "/fuwari",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
